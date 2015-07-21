be49450b814920fe4e14c6fdb2a12bf39a446337
Daniel Zwelling <dzwellingmusic@gmail.com>
2015-07-21 10:23:04 -0700

    Initial commit

---
+++ library.py
@@ -0,0 +1,20 @@
+# The library should be aware of a number of distinct shelves.
+# Each shelf should know what books it contains.
+# Create methods to add and remove a book from a self.
+# The library should have a method to report all books it contains.
+
+
+class Library(Object):
+
+    def __init__(self, shelf):
+        self.shelf = {}
+
+    def add_book(book):
+
+
+class Book(Library):
+
+    def __init__(self, title, author, genre):
+        self.title = title
+        self.author = author
+        self.genre = genre
