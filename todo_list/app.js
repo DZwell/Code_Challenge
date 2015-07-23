c7a29a2df94f4186ad7fce6dceb38dc6b8a09369
Daniel Zwelling <dzwellingmusic@gmail.com>
2015-07-22 17:10:45 -0700

    Moved Jquery to its own file

--- todo_list/app.js
+++ todo_list/app.js
@@ -1,10 +1,19 @@
-$(function(){
-    $('#submit').bind('click', function() {
+$(function() {
+
+    $('#submit').click(function() {
         var value = $('#input').val();
-        $('#list').append('<ul>' + value + '</ul>')
-    })
+        if(value == '') {
 
+        }
+        $('#list').prepend('<li>' + value + '</li>');
+        $('#input').val('');
+    });
 
-
-
-})
+     $('#input').keypress(function(e) {
+        var key = e.which;
+        if(key == 13) {
+            $('#submit').click();
+            return false;
+        }
+    });
+});
